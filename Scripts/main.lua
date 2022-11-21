e02c06650844db88322a91991a976af0fe532969
Andrey Lezhnin <andrey.lejnin@gmail.com>
2022-11-22 01:09:24 +0300

    removed unused method

--- Scripts/main.lua
+++ Scripts/main.lua
@@ -214,12 +214,6 @@ function OnEventAvatarCreated()
     end
 end
 
-function ToHex(str)
-    return (str:gsub('.', function (c)
-        return string.format('%02X', string.byte(c))
-    end))
-end
-
 function Init()
     if avatar and avatar.IsExist() then
         OnEventAvatarCreated()
